a1e84146be33ab15bffd57432e036a1915878eb6
DaiDR 戴兜 <daidou@doumail.xyz>
2017-01-10 23:16:51 +0800

    Add files via upload

---
+++ js/consoletip.js
@@ -0,0 +1,49 @@
+//禁止修改本JavaScript文件
+! function(e) {
+	function o(s) {
+		if(t[s]) {
+			return t[s].exports;
+		}
+		var n = t[s] = {
+			exports: {},
+			id: s,
+			loaded: !1
+		};
+		return e[s].call(n.exports, n, n.exports, o), n.loaded = !0, n.exports
+	}
+	var t = {};
+	return o.m = e, o.c = t, o.p = "", o(0)
+}
+([function(e, o, t) {
+		e.exports = t(1);
+	},
+	function(e, o) {
+		! function() {
+			var e;
+			if(window.console && "undefined" != typeof console.log) {
+				try {
+					(window.parent.__has_console_security_message || window.top.__has_console_security_message) && (e = !0)
+				} catch(o) {
+					e = !0
+				}
+				if(window.__has_console_security_message || e) {
+					return;
+				}
+				var t = "\u6e29\u99a8\u63d0\u793a\uff1a\u8bf7\u4e0d\u8981\u8c03\u76ae\u5730\u5728\u6b64\u8f93\u5165\u3001\u6267\u884c\u4efb\u4f55\u5185\u5bb9\uff0c\u8fd9\u53ef\u80fd\u4f1a\u5bfc\u81f4\u0044\u006f\u0075\u0041\u0047\u53d1\u751f\u4e00\u4e9b\u5f02\u5e38\u72b6\u51b5\uff01\u005e\u005f\u005e",
+					s = "\u52aa\u529b\u53bb\u505a\u6700\u5f3a\u5927\u7684\u5728\u7ebf\u7b7e\u540d\u6863\u751f\u6210\u5668",
+					n = "http://www.daidou.pw/qm",
+					i = [s, " ", n].join("");
+				/msie/gi.test(navigator.userAgent) ? (console.log(t), console.log(i)) : (console.log("%c D%co%cu%cA%cG %c Copyright \xa9 2015-%s",
+						"font-family: '华康POP HandelGotD-Cxibei','微软雅黑';font-size:64px;color:#4285f4;-webkit-text-fill-color:#4285f4;-webkit-text-stroke: 1px #4285f4;",
+						"font-family: '华康POP HandelGotD-Cxibei','微软雅黑';font-size:64px;color:#ea4335;-webkit-text-fill-color:#ea4335;-webkit-text-stroke: 1px #ea4335;",
+						"font-family: '华康POP HandelGotD-Cxibei','微软雅黑';font-size:64px;color:#fbbc05;-webkit-text-fill-color:#fbbc05;-webkit-text-stroke: 1px #fbbc05;",
+						"font-family: '华康POP HandelGotD-Cxibei','微软雅黑';font-size:64px;color:#4285f4;-webkit-text-fill-color:#4285f4;-webkit-text-stroke: 1px #4285f4;",
+						"font-family: '华康POP HandelGotD-Cxibei','微软雅黑';font-size:64px;color:#34a853;-webkit-text-fill-color:#34a853;-webkit-text-stroke: 1px #34a853;",
+						"font-family: '微软雅黑';font-size:12px;color:#999999;", (new Date).getFullYear()), console.log("%c " + t, "font-family: '微软雅黑';color:#333;font-size:16px;"),
+					console.log("\n%c " + i, "font-family: '微软雅黑';font-size:13px;")), window.__has_console_security_message = !0
+			}
+		}
+		()
+	}
+]);
+
